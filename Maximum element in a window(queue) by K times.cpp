9c9a45243c13292fa098645a23e5d6d81620236c
Dhruv Trehan <46952155+DhruvTrehan@users.noreply.github.com>
2020-06-16 23:13:17 +0530

    Update Maximum element in a window(queue) by K times.cpp

--- Maximum element in a window(queue) by K times.cpp
+++ Maximum element in a window(queue) by K times.cpp
@@ -1,44 +1,48 @@
-#include<iostream>
+#include <iostream>
 #include<deque>
-using namespace std; 
-void printKMax(int *arr , int n , int k)
-{
-	deque<int> q;
-	int i; 
-	for(i = 0 ; i<k ;i++)
-	{
-		while((!q.empty()) and arr[i] >= arr[q.back()])
-		{
-			q.pop_back();
-		}
-		q.push_back(i);
-	}
-	for(; i < n ; i++)
-	{
-		cout<<arr[q.front()]<<" ";
-		while((!q.empty() and q.front() <= i-k))
-		{
-			q.pop_front(); 
-		}
-		while((!q.empty()) and arr[i] >= arr[q.back()])
-		{
-			q.pop_back();
-		}
-	}
-	cout<<arr[q.front()];
-}
-int main()
-{
-	int n;
-	cin>>n;
-	int *arr = new int[n];
-	for(int i = 0 ; i < n ; i++)
-	{
-		cin>>arr[i];
-	}
-	int k;
-	cin>>k;
-	printKMax(arr,n,k);
-	return 0;
-}
+using namespace std;
 
+int main() {
+    int n;
+    int a[100000];
+    int k;
+    
+    cin>>n;
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+    cin>>k;
+    
+    //We have to process first k elements separtely
+    deque<int> Q(k);
+    int i;
+    for(i=0;i<k;i++){
+        
+        while(!Q.empty() &&a[i]>a[Q.back()]){
+            Q.pop_back();
+        }
+        Q.push_back(i);
+        
+    }
+    //Process the remaining elements 
+    for(;i<n;i++){
+        cout<<a[Q.front()]<<" ";
+        
+        //1. Remove the elements which are not the part of the window(Contraction)
+        while((!Q.empty() && (Q.front()<=i-k))){
+            Q.pop_front();
+        }
+        
+        //2. Remove the elements which are not useful and are in window
+        while(!Q.empty() && a[i]>=a[Q.back()]){
+            Q.pop_back();   
+        }
+        
+        //3. Add the new elements(Expansion)
+        Q.push_back(i);
+    }
+    
+    cout<<a[Q.front()]<<endl;
+    
+    
+    return 0;
