6141651a5e21e80740e8c63f8bde31ffb39ab99f
Dhruv Trehan <46952155+DhruvTrehan@users.noreply.github.com>
2020-06-23 16:32:32 +0530

    Update stack1.cpp

--- stack1.cpp
+++ stack1.cpp
@@ -5,26 +5,26 @@ using namespace std;
 class stack 
 {
 	private:
-		vector <int> v;
+		vector <int> v; //declaring a vector of type int 
 	public:
 		void push(int data)
 		{
-			v.push_back(data);
+			v.push_back(data);  //function to add data in vector 
 		}
 		bool empty()
 		{
-			return v.size()==0;
+			return v.size()==0;  //checking the size of the vector 
 		}
 		void pop()
 		{
 			if(!empty())
 			{
-				v.pop_back();	
+				v.pop_back();	//removing the elements from the vector 
 			}	
 		}
 		int top()
 		{
-			return v[v.size()-1];
+			return v[v.size()-1];  //returning the top most element from the vector 
 		}
 };
 	int main()
@@ -32,11 +32,11 @@ class stack
 		stack s; 
 		for(int i = 0 ; i<=5 ; i++)
 		{
-			s.push(i*i);
+			s.push(i*i);        //adding element in the stack 
 		}
 		while(!s.empty())
 		{
-			cout<<s.top()<<endl;
+			cout<<s.top()<<endl; //returning the top most element in the stack 
 			s.pop();
 		}
 		return 0;
